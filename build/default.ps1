76866343747cc0e90d68bac7f5b7b07516a86fe2
stefanoottaviani <stefano@smartmobile.it>
2022-01-20 10:19:46 +0100

    Build of nuget package: removed the '-build00000' parameter, that marks the package as pre-release.

--- build/default.ps1
+++ build/default.ps1
@@ -86,7 +86,8 @@ task Clean {
 task NuGetPack -depends Package {
     $versionString = Get-Version $assemblyInfoFilePath
 	$version = New-Object Version $versionString
-	$packageVersion = $version.Major.ToString() + "." + $version.Minor.ToString() + "." + $version.Build.ToString() + "-build" + $build_number.ToString().PadLeft(5,'0')
+	$buildNumberPrerelease = "" #"-build" + $build_number.ToString().PadLeft(5,'0')
+	$packageVersion = $version.Major.ToString() + "." + $version.Minor.ToString() + "." + $version.Build.ToString() + $buildNumberPrerelease
 	gci -r -i *.nuspec "$nuget_dir" |% { .$nuget_dir\nuget.exe pack $_ -basepath $base_directory -o $publish_directory -version $packageVersion }
 }
 
